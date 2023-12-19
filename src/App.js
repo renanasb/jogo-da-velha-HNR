890c93cbb20e66db3a54e7fb380a3764600f4d83
HenriqueVe <senaihenrique1@gmail.com>
2023-12-18 21:11:15 -0300

    aada

--- src/App.js
+++ src/App.js
@@ -198,6 +198,108 @@ function Board({ xIsNext, squares, onPlay }) {
           </div>
         </div>
       );
+    } else if (status === "Winner: X") {
+      return (
+        <div className="placar">
+          <div className="players" style={{ borderBottom: "2px solid white" }}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="19px"
+              height="19px"
+              fill="white  "
+              className="bi bi-x-lg"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30px"
+              height="30px"
+              fill="white"
+              className="bi bi-dash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+            </svg>
+          </div>
+          <div className="players">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18px"
+              height="18px"
+              fill="white"
+              className="bi bi-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30px"
+              height="30px"
+              fill="white"
+              className="bi bi-dash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+            </svg>
+          </div>
+        </div>
+      );
+    } else if (status === "Winner: O") {
+      return (
+        <div className="placar">
+          <div className="players">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="19px"
+              height="19px"
+              fill="white  "
+              className="bi bi-x-lg"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30px"
+              height="30px"
+              fill="white"
+              className="bi bi-dash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+            </svg>
+          </div>
+          <div className="players" style={{ borderBottom: "2px solid white" }}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18px"
+              height="18px"
+              fill="white"
+              className="bi bi-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30px"
+              height="30px"
+              fill="white"
+              className="bi bi-dash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+            </svg>
+          </div>
+        </div>
+      );
     } else {
       return (
         <div className="placar">
